89ba047c1f4a85903c729dc0df935e9e949d64c8
serhiivaluev-cpu <serhii.valuev@e-u.edu.ua>
2026-03-11 12:02:38 +0200

    Update staircase.rs

--- src/hackerrank/staircase.rs
+++ src/hackerrank/staircase.rs
@@ -1,1 +1,16 @@
+pub fn staircase(n: i32) -> String {
+    let mut result = String::new();
 
+    for i in 1..=n {
+        let spaces = " ".repeat((n - i) as usize);
+        let hashes = "#".repeat(i as usize);
+
+        result.push_str(&(spaces + &hashes));
+
+        if i != n {
+            result.push('\n');
+        }
+    }
+
+    result
+}
